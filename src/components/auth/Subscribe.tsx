cf31d56cf88cc9fb0ed1241cf27dbe99e20bd6ab
AlexeyAdoniev <Alexeya@xp.network>
2023-01-09 01:43:52 +0200

    sa

--- src/components/auth/Subscribe.tsx
+++ src/components/auth/Subscribe.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment  */
+
+/* eslint-disable no-constant-condition  */
+
+/* eslint-disable @typescript-eslint/no-unused-vars  */
+
 import React from "react";
 
 import subscribeIcon from "../../assets/img/subscribeIcon.png";
@@ -20,10 +26,12 @@ export const Subscribe = withServices(
     const dispatch = useDispatch();
     const [email, setEmail] = React.useState("");
     const [notanemail, setFail] = React.useState(false);
+    const [success, setSuccess] = React.useState(false);
 
     const { user, project } = useSelector((state: ReduxState) => ({
       user: state.global.userData,
       project: state.global.project,
+      justCompleted: state.global.justCompleted,
     }));
 
     const subscribeHandler = async () => {
@@ -41,25 +49,35 @@ export const Subscribe = withServices(
         ));
       console.log(res, "res");
       if (res) {
-        dispatch(setModal(undefined));
+        setSuccess(true);
+        //dispatch(setModal(undefined));
       }
     };
 
     return (
       <div className="subscribe">
         <img src={subscribeIcon} alt="subscribeIcon" />
-        <h2>Stay up to date</h2>
-        <p>Subscribe to XP.NETWORK news to complete the achievement</p>
-        <div className={`inputWrapper flexRow ${notanemail ? "failed" : ""}`}>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button onClick={subscribeHandler}>Subscribe</button>
-          <span>Not an email</span>
-        </div>
+        {success ? <h2>Thank you!</h2> : <h2>Stay up to date</h2>}
+        {success ? (
+          <p>
+            A magic link has been sent to your email to confirm your email
+            address
+          </p>
+        ) : (
+          <p>Subscribe to XP.NETWORK news to complete the achievement</p>
+        )}
+        {!success && (
+          <div className={`inputWrapper flexRow ${notanemail ? "failed" : ""}`}>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button onClick={subscribeHandler}>Subscribe</button>
+            <span>Not an email</span>
+          </div>
+        )}
       </div>
     );
   }
