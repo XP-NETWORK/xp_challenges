b4e63bb59217a5bc79991375d1b925805b719695
Alex Moshinsky <alexmoshinsky@Alexs-MacBook-Pro-40343.local>
2023-01-08 10:41:48 +0200

    footer mobile and desktop ready

--- src/pages/common/footer.tsx
+++ src/pages/common/footer.tsx
@@ -3,7 +3,8 @@ import { useRef, FC } from "react";
 import { withServices } from "../../hocs/withServices";
 
 import logo from "../../assets/img/icons/xplogoText.svg";
-
+import FooterTelegram from "../../assets/svgs/footer/telegramFooter.svg";
+import FooterTwitter from "../../assets/svgs/footer/footerTwitter.svg";
 
 const Footer: FC = () => {
   const background = useRef(null);
@@ -11,10 +12,9 @@ const Footer: FC = () => {
     <footer className="footer" ref={background} id="footer">
       <div className="container">
         <div className="row">
-          <div className="col-12">
-
-            <div className="media">
-              <div className="top flexRow">
+          <div className="col-12 desktopOnly">
+            <div className="media ">
+              <div className="top flexRow ">
                 <img src={logo} alt="xpLogo" className="logo" />
                 <ul className="flexRow links">
                   <li>
@@ -36,6 +36,29 @@ const Footer: FC = () => {
                   © {new Date().getFullYear()} XP.NETWORK Ltd. All Rights
                   Reserved
                 </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 mobileOnly">
+            <div className="media mobileWarper">
+              <div className="bot">
+                <div className="mobileContainerFlex">
+                  <div className="footerFlex">
+                    <img src={FooterTelegram} className="footerTelegram" alt="FooterTelegram" />
+                    <img src={FooterTwitter} alt="FooterTwitter" />
+                  </div>
+                  <div className="footerFlex">
+                    <img
+                      src={logo}
+                      alt="xpLogo"
+                      className="logo mobileXpLogo"
+                    />
+                    <div className="yearFooterMobileText">
+                      {" "}
+                      © {new Date().getFullYear()}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
