4d4994327c7c86ca088751a04ae1766d89cbceba
rony derra <derrarony@gmail.com>
2023-01-19 17:20:06 +0200

    Add an icon to the “Connect with Telegram” button - rony done

--- src/components/auth/TelegramButton.tsx
+++ src/components/auth/TelegramButton.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { saveTelegramUniqueId } from "utils";
 import { v4 as uuidv4 } from "uuid";
+import Vector from "../../assets/svgs/singup/Vector.svg";
 
 export const TelegramButton = ({ classes }: { classes?: string }) => {
   const unique = uuidv4();
   const openInNewTab = () => {
-    window.open(
-      `https://t.me/challengeRegestrationBot/?start=${unique}`,
-      "_blank",
-      "noreferrer"
-    );
+    window.open(`https://t.me/challengeRegestrationBot/?start=${unique}`, "_blank", "noreferrer");
     saveTelegramUniqueId(unique);
     localStorage.removeItem("CHALLENGE_EXIST");
   };
 
   return (
     <button className={classes ? classes : "accent"} onClick={openInNewTab}>
-      Connect with Telegram
+      <span>
+        <img src={Vector} />
+        <span> Connect with Telegram</span>
+      </span>
     </button>
   );
 };
