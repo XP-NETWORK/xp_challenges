7bbce024dac569622f327133c31f5d63ae279d99
rony derra <derrarony@gmail.com>
2023-01-22 12:36:50 +0200

    AchievmentLoader

---
+++ src/components/achievmentLoader/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import "./achievmentLoader.css";
+
+export const AchievmentLoader: FC = () => {
+  return (
+    <>
+      <div className="achievmentLoaderContainer">
+        <span className="achievmentLoader"></span>
+      </div>
+    </>
+  );
+};
