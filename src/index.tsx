d512ce442863ed6b2502457126c74664d101f188
rony derra <derrarony@gmail.com>
2023-02-02 11:30:37 +0200

    _DEFAULT_TELEGRAM_LINK

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ import "./assets/styles/media.css";
 export const config = {
   _DEFAULT_TWITTER_LINK: "https://twitter.com/xpnetwork_",
   _DEFAULT_BRIDGE_LINK: "https://staging-bridge.xp.network?xpchallenge=true",
-  _DEFAULT_TELEGRAM_LINK: "https://t.me/+K5jWhw43C-ZjZmE0",
+  _DEFAULT_TELEGRAM_LINK: "https://t.me/+N2aUEx2RqIo2NDU0",
   _TWITTER_AUTH: "https://support-bot-xp.herokuapp.com/twitterLogin",
   _WIDGET: "https://widget.xp.network", //https://widget-staging.xp.network
   _WIDGET_ID: "63bc17c8a65fd4aaf0312526",
