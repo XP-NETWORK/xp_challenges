ad0109fceff8885f3cdc0e8c01c486d49f975fff
rony derra <derrarony@gmail.com>
2023-02-13 15:21:54 +0200

    TelegramButtonV2

--- src/components/auth/TelegramButton.tsx
+++ src/components/auth/TelegramButton.tsx
@@ -6,7 +6,7 @@ import Vector from "../../assets/svgs/singup/Vector.svg";
 export const TelegramButton = ({ classes }: { classes?: string }) => {
   const unique = uuidv4();
   const openInNewTab = () => {
-    window.open(`https://t.me/challengeRegestrationBot/?start=${unique}`, "_blank", "noreferrer");
+    window.open(`https://t.me/XpRegDemobot/?start=${unique}`, "_blank", "noreferrer");
     saveTelegramUniqueId(unique);
     localStorage.removeItem("CHALLENGE_EXIST");
   };
--- src/components/auth/TelegramButtonV2.tsx
+++ src/components/auth/TelegramButtonV2.tsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 export const TelegramButtonV2 = ({ classes }: { classes?: string }) => {
   const unique = uuidv4();
   const openInNewTab = () => {
-    window.open(`https://t.me/challengeRegestrationBot/?start=${unique}`, "_blank", "noreferrer");
+    window.open(`https://t.me/XpRegDemobot/?start=${unique}`, "_blank", "noreferrer");
     saveTelegramUniqueId(unique);
     localStorage.removeItem("CHALLENGE_EXIST");
   };
